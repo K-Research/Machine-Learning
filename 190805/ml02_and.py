8e1914f610e188c393a2bc7514c78853a5fdc621
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-08-05 09:32:22 +0900

    Add files via upload

---
+++ 190805/ml02_and.py
@@ -0,0 +1,19 @@
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+
+# 1. 데이터
+X_data = [[0, 0], [1, 0], [0, 1], [1, 1]]
+y_data = [0, 0, 0, 1]
+
+# 2. 모델
+model = LinearSVC()
+
+# 3. 실행
+model.fit(x_data, y_data)
+
+# 4. 평가 예측
+x_test = [[0, 0], [1, 0], [0, 1], [1, 1]]
+y_predict = model.predict(x_test)
+
+print(y_predict, "의 예측 결과 : ", y_predict)
+print("acc = ", accuracy_score([0, 0, 0, 1], y_predict))
