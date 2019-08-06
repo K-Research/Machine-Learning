3f811b7f62839fcdad6b4f9176c266b922bb2e1f
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-08-06 15:11:29 +0900

    Add files via upload

--- 190806/ml07_linear.py
+++ 190806/ml07_linear.py
@@ -1,9 +1,14 @@
+from sklearn.datasets import load_boston
 
-
-
-
-
-
+boston = load_boston()
+print(boston.data.shape)
+print(boston.keys())
+print(boston.target)
+print(boston.target.shape)
 
 x = boston.data
 y = boston.target
+
+print(type(boston))
+
+from sklearn.lenear_model import LinearRegression # Lasso, Ridge
