5aa7f7646fe100aaeb81942c0f90b65e9aea44c2
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-08-08 16:11:20 +0900

    Add files via upload

--- 190808/ml25_gradientboost.py
+++ 190808/ml25_gradientboost.py
@@ -38,3 +38,25 @@ y_pred = model.predict(x_test)
 print(classification_report(y_test, y_pred))
 print("정답률 = ", accuracy_score(y_test, y_pred))
 print(score)
+
+# 최적의 매개 변수 =  GradientBoostingClassifier(criterion='friedman_mse', init=None,
+#               learning_rate=0.1, loss='deviance', max_depth=None,
+#               max_features='sqrt', max_leaf_nodes=None,
+#               min_impurity_decrease=0.0, min_impurity_split=None,
+#               min_samples_leaf=1, min_samples_split=2,
+#               min_weight_fraction_leaf=0.0, n_estimators=100,
+#               n_iter_no_change=None, presort='auto', random_state=0,
+#               subsample=1.0, tol=0.0001, validation_fraction=0.1,
+#               verbose=0, warm_start=False)
+#               precision    recall  f1-score   support
+
+#            0       0.67      0.25      0.36        32
+#            1       0.95      1.00      0.97       910
+#            2       1.00      0.50      0.67        38
+
+#    micro avg       0.95      0.95      0.95       980
+#    macro avg       0.87      0.58      0.67       980
+# weighted avg       0.95      0.95      0.94       980
+
+# 정답률 =  0.9520408163265306
+# 0.9520408163265306
