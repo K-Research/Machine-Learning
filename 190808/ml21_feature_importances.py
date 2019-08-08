37a275e7b7894cd29c469a932f297a1f42aa46c3
Taehwan-Lab <52855867+Taehwan-Lab@users.noreply.github.com>
2019-08-08 11:38:23 +0900

    Add files via upload

--- 190808/ml21_feature_importances.py
+++ 190808/ml21_feature_importances.py
@@ -16,3 +16,17 @@ print("테스트 세트 정확도 : {:.3f}".format(tree.score(X_test, Y_test)))
 # print("테스트 세트 정확도 : {:.3f}".format(tree.score(X_test, Y_test)))
 
 print("특성 중요도 : \n", tree.feature_importances_)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_feature_importances_cancer(model):
+    n_features = cancer.data.shape[1]
+    plt.barh(np.arange(n_features), model.feature_importances_, align = 'center')
+    plt.yticks(np.arange(n_features), cancer.feature_names)
+    plt.xlabel("특성 중요도")
+    plt.ylabel("특성")
+    plt.ylim(-1, n_features)
+
+plot_feature_importances_cancer(tree)
+plt.show()
