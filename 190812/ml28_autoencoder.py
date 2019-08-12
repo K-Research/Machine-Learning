7ee3c1c2a818bacd86d1d8f45a028c064ddb6afe
Taewhan Kim <lechevalier@naver.com>
2019-08-12 12:11:43 +0900

    Add files via upload

--- 190812/ml28_autoencoder.py
+++ 190812/ml28_autoencoder.py
@@ -49,6 +49,7 @@ encoder.summary()
 decoder.summary()
 
 autoencoder.compile(optimizer = 'adadelta', loss = 'binary_crossentropy', metrics = ['accuracy'])
+# autoencoder.compile(optimizer = 'adadelta', loss = 'mse', metrics = ['accuracy'])
 
 history = autoencoder.fit(x_train, x_train, epochs = 50, batch_size = 256, shuffle = True, validation_data = (x_test, x_test))
 
